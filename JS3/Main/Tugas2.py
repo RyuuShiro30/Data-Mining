98cde0e19fa109110819032706d72c5fbf18d201
RyuuShiro30 <annoraegaprasetyo@gmail.com>
2026-04-18 16:21:27 +0700

    Revisi Tugas 2 - JS Regression

--- JS3/Main/Tugas2.py
+++ JS3/Main/Tugas2.py
@@ -1,5 +1,5 @@
 # Tugas Nomor 2 Data Mining - Regression Logistic Model
-
+# Import Library
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,9 +16,12 @@ print("Missing Values:\n", df.isnull().sum())
 df.fillna(df.median(), inplace=True)
 
 X = df.drop('TenYearCHD', axis=1)
-y = df['TenYearCHD']   
+y = df['TenYearCHD']
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 scaler = StandardScaler()
+
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
